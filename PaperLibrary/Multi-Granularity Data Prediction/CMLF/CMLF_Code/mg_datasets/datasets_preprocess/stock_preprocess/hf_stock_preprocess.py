2289b08f397e1c6720fc2647d14793e61978b173
KarryRen <KarryRenKai@outlook.com>
2024-04-10 15:11:00 +0800

    finsih data preprocess

--- PaperLibrary/Multi-Granularity Data Prediction/CMLF/CMLF_Code/mg_datasets/datasets_preprocess/stock_preprocess/hf_stock_preprocess.py
+++ PaperLibrary/Multi-Granularity Data Prediction/CMLF/CMLF_Code/mg_datasets/datasets_preprocess/stock_preprocess/hf_stock_preprocess.py
@@ -2,91 +2,98 @@
 # @Time    : 2024/4/9 16:13
 # @Author  : Karry Ren
 
-""""""
+""" Please follow the `README.md` to download the 1-day and 1-min stock dataset,
+      and get the directory like:
+        /Users/username/.qlib/qlib_data/qlib_data/
+            ├── cn_data
+                ├── calendars
+                ├── features
+                └── instruments
+            ├── cn_data_1min
+                ├── calendars
+                ├── features
+                └── instruments
+Please make sure you have this directory and the dataset is right !!!
+Please run `python daily_stock_preprocess.py` and get the daily dataset, firstly !!!
+
+During the preprocessing, we wil do operations by the following steps:
+    1. Change the `PATH` and `daily_stock_df_name` based on your situation.
+    2. Read the preprocessed daily stock data.
+    3. Prepare the param for hf stock data.
+    4. For-Loop to compute high-frequency data.
+
+"""
 
 import numpy as np
 import pandas as pd
-
+import argparse
 from functools import reduce
 from tqdm import tqdm
-import qlib
-from qlib.data import D
 import copy
 import datetime as dt
+import qlib
+from qlib.data import D
 
-qlib.init()
+# ---- Build up the parser ---- #
+parser = argparse.ArgumentParser()
+parser.add_argument("--hf", type=str, default="15_min",
+                    help="The high frequency, you have only 4 choices: `1_min`, `5_min`, `15_min`, `1_hour` !!")
+args = parser.parse_args()
 
-print('Reading...')
-label_names = ['LABEL0']
-data = pd.read_pickle('./data/day_csi300_till_20201228.pkl').loc(axis=0)['2007-01-01':, :]
-ret = data[label_names]
-stocks = ret.reset_index().groupby('instrument')['datetime'].agg(['min', 'max'])
-print(stocks)
-print('Read over')
-N = 15  # 5 min
-assert 240 % N == 0
-assert N != 240
-num_bar = 240 // N
-print(f'num_bar {num_bar}')
-res = dict()
-for code, (start_time, end_time) in tqdm(stocks.iterrows()):
-    # print(code)
-    df = D.features([code], ['$high', '$low', '$close', '$open', '$volume', '$money'],
-                    start_time='%s 09:31:00' % str(start_time)[:10],
-                    end_time='%s 15:00:00' % str(end_time)[:10],
-                    freq='1min', disk_cache=0)
-    if df.empty:
-        print('WARN: %s not found' % code)
+# ---- Step 1. Change the `PATH` and `daily_stock_df_name` based on your situation ---- #
+# please make sure the `PATH` and `daily_stock_df_name` are same as `daily_stock_preprocess.py` !!!
+CSI300_STOCK_DATASET_PATH, daily_stock_df_name = "../../../../CMLF_Dataset/CSI300", "1_day.pkl"
+# read the high_frequency
+high_frequency = args.hf
+print(f"************************** BEGIN HIGH-FREQUENCY `{high_frequency}` CSI300 STOCK DATASET PREPROCESSING **************************")
+
+# ---- Step 2. Read the preprocessed daily stock data ---- #
+print(f"************************** BEGIN READING THE DAILY STOCK DATA **************************")
+label_name_list = ["LABEL"]
+daily_stock_df = pd.read_pickle(f"{CSI300_STOCK_DATASET_PATH}/{daily_stock_df_name}")
+daily_stock_labels = daily_stock_df[label_name_list]  # read the daily labels
+# EXCELLENT STOCK-CODE, MIN-TRADING-DATE, MAX-TRADING-DATE !!!
+stock_code_trading_date_df = daily_stock_labels.reset_index().groupby("instrument")["datetime"].agg(["min", "max"])
+print(f"************************** FINISH READING THE DAILY STOCK DATA **************************")
+
+# ---- Step 3. Prepare the param for hf stock data ---- #
+print(f"************************** BEGIN PREPARING THE PARAM **************************")
+# set the tick gap based on the high_frequency
+if high_frequency == "1_min":
+    tick_gap = 1  # 1 min
+elif high_frequency == "5_min":
+    tick_gap = 5  # 5 min
+elif high_frequency == "15_min":
+    tick_gap = 15  # 15 min
+elif high_frequency == "1_hour":
+    tick_gap = 60  # 1 hour
+else:
+    raise TypeError(high_frequency)
+# compute the tick num of one day
+tick_num = 240 // tick_gap
+print(f"************************** BEGIN PREPARING THE PARAM, TICK_NUM={tick_num} **************************")
+
+# ---- Step 4. For-Loop to compute high frequency data ---- #
+print(f"************************** BEGIN COMPUTING THE HIGH FREQUENCY DATA **************************")
+qlib.init(provider_uri={"1min": "/Users/karry/.qlib/qlib_data/cn_data_1min"})  # init the q lib
+for stock_code, (start_date, end_date) in tqdm(stock_code_trading_date_df.iterrows()):  # for loop to get each stock code
+    # read the minute stock
+    minute_stock_df = D.features(
+        instruments=[stock_code],
+        fields=['$high', '$low', '$close', '$open', '$volume'],
+        # start_time=f"{str(start_date)[:10]} 09:31:00", end_time=f"{str(end_date)[:10]} 15:00:00",
+        freq="1min", disk_cache=0
+    )
+    # test have the stock or not
+    if minute_stock_df.empty:
+        print(f"WARNING: The minute stock data of {stock_code} not found !!!")
         continue
-    df = df.loc[code]
-    if N > 1:
-        # df = df.groupby(df.index.ceil('%smin'%N)).agg(
-        #     {'$high': 'max', '$low': 'min', '$close': 'last', '$open':'first','$volume': 'sum','$money':'sum'})
-        if N == 30:
-            index_arr = np.array([])
-            for day in np.unique(df.index.date):
-                index_arr = np.append(index_arr, \
-                                      np.array([dt.datetime(day.year, day.month, day.day, 10, 30, 0, 0)] * N + \
-                                               [dt.datetime(day.year, day.month, day.day, 11, 30, 0, 0)] * N + \
-                                               [dt.datetime(day.year, day.month, day.day, 14, 0, 0, 0)] * N + \
-                                               [dt.datetime(day.year, day.month, day.day, 15, 0, 0, 0)] * N))
-            df_index = pd.to_datetime(index_arr)
-            df = df.groupby(df_index).agg(
-                {'$high': 'max', '$low': 'min', '$close': 'last', '$open': 'first', '$volume': 'sum', '$money': 'sum'})
-        else:
-            df = df.groupby(df.index.ceil('%smin' % N)).agg(
-                {'$high': 'max', '$low': 'min', '$close': 'last', '$open': 'first', '$volume': 'sum', '$money': 'sum'})
-
-    X = df.values.reshape(-1, num_bar, len(df.columns))  # [*,*,h/l/c/o/vol/vwap]
-    close = copy.deepcopy(X[:, -1:, 2])  # last close
-    volume = np.mean(X[:, :, 4], axis=1, keepdims=True)  # mean volume
-    for i in range(4):
-        X[:, :, i] = copy.deepcopy(X[:, :, i] / close)
-    X[:, :, 5] = copy.deepcopy(X[:, :, 5] / (X[:, :, 4] + 1e-12))  # vwap = money / volume
-    X[:, :, 5] = copy.deepcopy(X[:, :, 5] / close)  # vwap norm by last close
-
-    X[:, :, 4] = copy.deepcopy(X[:, :, 4] / (volume + 1e-12))  # volume norm by mean volume
-
-    new_df = pd.DataFrame(X.reshape(-1, num_bar * len(df.columns)),
-                          index=pd.to_datetime(df.index[::num_bar].date))
-    columns = ['$high', '$low', '$close', '$open', '$volume', '$vwap']
-    new_df.columns = reduce(lambda a, b: a + b, [
-        ['%s_%d' % (s[1:], d) for s in columns]
-        for d in range(1, num_bar + 1)
-    ])
-    new_df = new_df[new_df.columns.values.reshape(-1, len(columns)).T.flatten().tolist()]
-    res[code] = copy.deepcopy(new_df)
-
-df = pd.concat(res, axis=0)
-df.index.names = ['instrument', 'datetime']
-df.index = df.index.swaplevel()  # ['instrument', 'datetime'] -> ['datetime', 'instrument' ]
-df = df.reindex(ret.index)
-
-# df['LABEL0'] = ret
-ret = ret.reindex(df.index)
-for item in label_names:
-    df[item] = ret[item].values
-
-df = df.sort_index()
-print(df)
-df.to_pickle('../data/hft_%dm_csi300.pkl' % N)
+    # just get the minute stock df
+    minute_stock_df = minute_stock_df.loc[stock_code]
+    if tick_gap > 1:  # other tick gap
+        hf_stock_df = minute_stock_df.groupby(minute_stock_df.index.ceil(f"{tick_gap}min")).agg(
+            {"$high": "max", "$low": "min", "$close": "last", "$open": "first", "$volume": "sum"}
+        )
+    else:
+        hf_stock_df = minute_stock_df  # keep raw
+    print(hf_stock_df)
--- PaperLibrary/Multi-Granularity Data Prediction/CMLF/CMLF_Code/mg_datasets/datasets_preprocess/stock_preprocess/stock_download.sh
+++ PaperLibrary/Multi-Granularity Data Prediction/CMLF/CMLF_Code/mg_datasets/datasets_preprocess/stock_preprocess/stock_download.sh
@@ -3,8 +3,8 @@
 
 # download the stock dataset
 
-# get 1d data
+# download 1-day data
 python -m qlib.run.get_data qlib_data --target_dir ~/.qlib/qlib_data/cn_data --region cn
 
-# get 1min data
+# download 1-min data
 python -m qlib.run.get_data qlib_data --target_dir ~/.qlib/qlib_data/cn_data_1min --region cn --interval 1min
